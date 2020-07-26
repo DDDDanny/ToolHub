cf1bba3884797156c3d33a0b8d52acc7e06b43c1
DDDDanny <707956456@qq.com>
2020-07-26 23:51:26 +0800

    feat: 完成生成随机日期时间接口

--- FakerData/FakerData.py
+++ FakerData/FakerData.py
@@ -4,6 +4,9 @@
 # @File    : FakerData.py
 # @describe: 随机生成数据逻辑处理
 
+import datetime
+from random import Random
+
 from faker import Faker
 
 
@@ -21,7 +24,7 @@ class FakerData(object):
             {"id": 100, "catName": "姓名"},
             {"id": 101, "catName": "地址"},
             {"id": 102, "catName": "手机号"},
-            {"id": 103, "catName": "车牌号"},
+            {"id": 103, "catName": "日期时间"},
             {"id": 104, "catName": "公司名称"},
         ]
         return self.faker_list
@@ -30,7 +33,7 @@ class FakerData(object):
     def faker_random(self, faker_id, attr):
         # res 用于存储假数据生成的结果
         res = ''
-        # 生成假姓名
+        # 生成随机姓名
         if faker_id == '100':
             if attr['lastName'] == '' and attr['firstName'] == '':
                 res = self.faker_obj.name()
@@ -40,7 +43,7 @@ class FakerData(object):
                 res = self.faker_obj.last_name() + attr['firstName']
             else:
                 res = attr['lastName'] + attr['firstName']
-        # 生成假地址
+        # 生成随机地址
         elif faker_id == '101':
             attr['country'] = self.faker_obj.country() if attr['country'] == '' else attr['country']  # 生成国家
             attr['province'] = self.faker_obj.province() if attr['province'] == '' else attr['province']  # 生成省
@@ -53,10 +56,29 @@ class FakerData(object):
             print(address_list)
             for address in address_list:
                 res += address
-        # 生成假电话号码
+        # 生成随机电话号码
         elif faker_id == '102':
             if attr['prefix'] == '':
                 res = self.faker_obj.phone_number()
             else:
                 res = attr['prefix'] + self.faker_obj.phone_number()[3:]
+        # 生成随机日期时间
+        elif faker_id == '103':
+            attr['year'] = self.faker_obj.year() if attr['year'] == '' else attr['year']
+            attr['month'] = self.faker_obj.month() if attr['month'] == '' else attr['month']
+            attr['day'] = self.faker_obj.day_of_month() if attr['day'] == '' else attr['day']
+            attr['hour'] = str(Random().randint(0, 23)).zfill(2) if attr['hour'] == '' else attr['hour']
+            attr['minute'] = str(Random().randint(0, 59)).zfill(2) if attr['minute'] == '' else attr['minute']
+            attr['seconds'] = str(Random().randint(0, 59)).zfill(2) if attr['seconds'] == '' else attr['seconds']
+            date_str = attr['year'] + '-' + attr['month'] + '-' + attr['day']
+            time_str = attr['hour'] + ':' + attr['minute'] + ':' + attr['seconds']
+            week_list = [
+                '星期日（Sunday）', '星期一（Monday）', '星期二（Tuesday）', '星期三（Wednesday）',
+                '星期四（Thursday）', '星期五（Friday）', '星期六（Saturday）', '无此日（No such day）'
+            ]
+            try:
+                week = int(datetime.datetime(int(attr['year']), int(attr['month']), int(attr['day'])).strftime('%w'))
+            except Exception:
+                week = 7
+            res = date_str + ' ' + time_str + ' ' + week_list[week]
         return {'result': res}
