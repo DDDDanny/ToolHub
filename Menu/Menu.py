252435ff431a2ecd9bbe95bd63b661df6280b05d
DDDDanny <707956456@qq.com>
2020-07-27 22:02:00 +0800

    feat: 调整主菜单目录结构

--- Menu/Menu.py
+++ Menu/Menu.py
@@ -13,29 +13,14 @@ def menu_faker_list():
         'path': None,
         'children': [
             {
-                'nodeName': '基础类 (Base)',
+                'nodeName': '生成随机数据',
                 'iconName': 'mdi-creation',
-                'path': '/faker/base',
+                'path': '/faker/dataGenerate',
             },
             {
-                'nodeName': '人物类 (Human)',
-                'iconName': 'mdi-account-heart',
-                'path': '/faker/human',
-            },
-            {
-                'nodeName': '地址类 (Address)',
-                'iconName': 'mdi-directions-fork',
-                'path': '/faker/address',
-            },
-            {
-                'nodeName': '金融类 (Finance)',
-                'iconName': 'mdi-currency-usd-circle',
-                'path': '/faker/finance',
-            },
-            {
-                'nodeName': '其他类 (Other)',
-                'iconName': 'mdi-cards-heart',
-                'path': '/faker/other',
+                'nodeName': '导出随机数据',
+                'iconName': 'mdi-file-export',
+                'path': '/faker/dataExport',
             },
         ]
     }
