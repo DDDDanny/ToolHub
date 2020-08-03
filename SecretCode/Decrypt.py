b91739d80e9bc492117dfeeb7814f753e922964a
DDDDanny <707956456@qq.com>
2020-08-03 22:40:20 +0800

    feat: 完成数据解密接口

--- SecretCode/Decrypt.py
+++ SecretCode/Decrypt.py
@@ -2,4 +2,32 @@
 # @Time    : 2020/7/29 20:05
 # @Author  : DannyDong
 # @File    : Decrypt.py
-# @describe: 
+# @describe: 数据解密处理
+
+import base64
+
+
+class Decrypt(object):
+    def __init__(self, wait_str):
+        self.waiting_str = wait_str
+
+    # Base64编码
+    def base64_code(self):
+        new_str = self.waiting_str.encode('utf-8')  # 需要转成bytes字节符
+        new_str = base64.b64decode(new_str)
+        return new_str.decode()
+
+
+# 处理数据解密业务
+def go_decrypt(cate, wait_str):
+    init_obj = Decrypt(wait_str)
+    # cate=1: Base64;
+    if cate == '1':
+        res = init_obj.base64_code()
+    else:
+        raise {'result': '加密类型选择错误'}
+    return {'result': res}
+
+
+if __name__ == '__main__':
+    print(go_decrypt('1', 'V2FpdGluZyBTdHJpbmc='))
--- SecretCode/SecretCodeBlue.py
+++ SecretCode/SecretCodeBlue.py
@@ -8,6 +8,7 @@ from flask import Blueprint, request
 
 from Common.ReCommon import Common
 from SecretCode.Encrypt import go_encrypt
+from SecretCode.Decrypt import go_decrypt
 
 secretCode = Blueprint('secretCode', __name__)
 
@@ -28,3 +29,16 @@ def code_encrypt():
         else:
             res = go_encrypt(cate, wait_str, salt)
             return Common.success_json(res)
+
+
+@secretCode.route('/secretCode/goDecrypt', methods=['POST'])
+def code_decrypt():
+    form_data = eval(request.get_data(as_text=True))
+    print(form_data)
+    cate = form_data['cate']
+    wait_str = form_data['waitStr']
+    if cate == '' or wait_str == '':
+        return Common.fail_json()
+    else:
+        res = go_decrypt(cate, wait_str)
+        return Common.success_json(res)
